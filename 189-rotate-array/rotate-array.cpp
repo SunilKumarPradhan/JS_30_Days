88a0e092dfc62c3f638bdf31c5902def2c899979
sunilkumarpradhan <89890497+SunilKumarPradhan@users.noreply.github.com>
2025-03-09 16:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (97.26%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,14 +4,9 @@ public:
         int n=nums.size();
         k= k % n;
 
-        vector<int>vr(nums.end()-k , nums.end());
-        for(int i=n-1;i>=k;i--){
-            nums[i]=nums[i-k];
-        }
-
-        for(int i=0;i<k;i++){
-            nums[i]=vr[i];
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
 
